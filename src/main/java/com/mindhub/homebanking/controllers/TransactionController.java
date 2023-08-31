8ba8879975850f3a697a423b027b7c9d523f168a
JimenaCarracedo <jime_nmc@hotmail.com>
2023-08-31 10:23:00 -0300

    import Transactional

--- src/main/java/com/mindhub/homebanking/controllers/TransactionController.java
+++ src/main/java/com/mindhub/homebanking/controllers/TransactionController.java
@@ -9,12 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
