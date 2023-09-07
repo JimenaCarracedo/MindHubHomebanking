5e14a7d1bf5519c5cfb941a1a64c3742c50b4a36
JimenaCarracedo <jime_nmc@hotmail.com>
2023-09-07 18:55:55 -0300

    deploy

--- src/test/java/com/mindhub/homebanking/ClientRepositoryTests.java
+++ src/test/java/com/mindhub/homebanking/ClientRepositoryTests.java
@@ -2,9 +2,8 @@ package com.mindhub.homebanking;
 
 import com.mindhub.homebanking.models.Card;
 import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.models.Loan;
 import com.mindhub.homebanking.repositories.CardRepository;
-import com.mindhub.homebanking.repositories.LoanRepository;
+import com.mindhub.homebanking.repositories.ClientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -19,32 +18,16 @@ import static org.hamcrest.Matchers.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class CardRepositoryTests {
+public class ClientRepositoryTests {
     @Autowired
-    CardRepository cardRepository;
+    ClientRepository clientRepository;
     @Test
-    public void existCards(){
+    public void existClient(){
 
-        List<Card> cards = cardRepository.findAll();
+        List<Client> clients = clientRepository.findAll();
 
-        assertThat(cards,is(not(empty())));
+        assertThat(clients,is(not(empty())));
 
     }
-
-
-
-    @Test
-
-    public void existPersonalCard(){
-
-        List<Card> cards = cardRepository.findAll();
-
-
-        assertThat(cards, hasItem(hasProperty("type", is(DEBIT))));
-        assertThat(cards, hasItem(hasProperty("type", is(CREDIT))));
-
-    }
-
-
 
 }
