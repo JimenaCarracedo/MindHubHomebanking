d812f24b856e087e689b1033057dc22086f91fcb
JimenaCarracedo <jime_nmc@hotmail.com>
2023-09-09 13:29:09 -0300

    thymeleaf.prefix in application.properties

--- src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
+++ src/main/java/com/mindhub/homebanking/configurations/WebAuthorization.java
@@ -20,7 +20,7 @@ public class WebAuthorization{
     @Bean
     protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
-                .antMatchers(HttpMethod.GET, "/web/index" ).permitAll()
+                .antMatchers(HttpMethod.GET, "/web/index.html" ).permitAll()
                 .antMatchers(HttpMethod.GET, "/api/clients/current/**").hasAnyAuthority("ADMIN", "CLIENT")
                 .antMatchers(HttpMethod.POST, "/api/clients/current/**").hasAnyAuthority("ADMIN", "CLIENT")
                 .antMatchers(HttpMethod.GET, "/web/accounts.html").hasAnyAuthority("ADMIN", "CLIENT")
